2069142d1283357139d93fbf887ac439c5dd2b0a
Lina <81854712+kliina@users.noreply.github.com>
2021-12-19 23:11:37 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <title>Réponse du serveur</title>
+  </head>
+  <body>
+  <?php print "Hello !";
+  print $_POST["nom"] ;
+  print " ";
+  print $_POST["prenom"];
+  print "<BR><BR><HR>";
+    print "We received your message!👍";
+  ?>
+  </body>
+</html>
